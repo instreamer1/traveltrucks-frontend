d7684fd009a2f43150070835fbef382331d9104c
Aleksander <aleksander.0705@gmail.com>
2024-10-20 20:16:59 +0300

    add pagination on backend

--- src/components/SaidBar/SaidBar.jsx
+++ src/components/SaidBar/SaidBar.jsx
@@ -53,12 +53,12 @@ const SaidBar = () => {
       return;
     }
     setShowWarning(false);
-    dispatch(fetchCampers(filters)); 
+    dispatch(fetchCampers(filters));
   };
 
   const handleReset = () => {
     dispatch(resetFilters());
-    dispatch(fetchCampers(null));
+    dispatch(fetchCampers());
   };
 
   return (
@@ -172,9 +172,10 @@ const SaidBar = () => {
             <path d='M0 1H360' stroke='#DADDE1' />
           </svg>
           <div className={css.filterOptionsWrap}>
-            <label   className={`${css.filterOption} ${
-              filters.vehicleType === 'alcove' ? css.selected : ''
-            }`}>
+            <label
+              className={`${css.filterOption} ${
+                filters.vehicleType === 'alcove' ? css.selected : ''
+              }`}>
               <input
                 type='radio'
                 name='vehicleType'
@@ -186,10 +187,11 @@ const SaidBar = () => {
                 <use href={`${iconSprite}#icon-bi_grid-1x2`}></use>
               </svg>
               Van
-            </label >
-            <label  className={`${css.filterOption} ${css.filterOptionIntegr}  ${
-              filters.vehicleType === 'fullyIntegrated' ? css.selected : ''
-            }`}>
+            </label>
+            <label
+              className={`${css.filterOption} ${css.filterOptionIntegr}  ${
+                filters.vehicleType === 'fullyIntegrated' ? css.selected : ''
+              }`}>
               <input
                 type='radio'
                 name='vehicleType'
@@ -202,9 +204,10 @@ const SaidBar = () => {
               </svg>
               Fully Integrated
             </label>
-            <label  className={`${css.filterOption} ${
-              filters.vehicleType === 'panelTruck' ? css.selected : ''
-            }`}>
+            <label
+              className={`${css.filterOption} ${
+                filters.vehicleType === 'panelTruck' ? css.selected : ''
+              }`}>
               <input
                 type='radio'
                 name='vehicleType'
@@ -229,7 +232,7 @@ const SaidBar = () => {
               type='button'
               className={css.searchBtn}
               onClick={handleReset}>
-               {isLoading ? 'Reset Filters...' : 'Reset Filters'}
+              {isLoading ? 'Reset Filters...' : 'Reset Filters'}
             </button>
           )}
           {showWarning && <p className={css.warningMessage}>Select filters</p>}
